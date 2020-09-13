bbd6a006f32cef8e254e2ef880bbc69a3dcae48d
Zahid Uddin <zahidudd0.in@gmail.com>
2020-09-13 14:31:28 -0800

    Change Index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -23,7 +23,7 @@
         @foreach ($errors->all() as $error)
             <div class="alert alert-danger alert-dismissible">
                 <button type="button" class="close" data-dismiss="alert">&times;</button>
-                <strong>Whoops!</strong> {{$error}}
+                <strong>Whoops!!!</strong> {{$error}}
             </div>
         @endforeach
         @endif
